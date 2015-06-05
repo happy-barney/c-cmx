ad676ec7094589d177e723122969bb13d838b4e1
Branislav Zahradnik <Branislav.Zahradnik@dimoco.at>
2015-06-05 15:23:13 +0200

    Fix binding predicates

--- cmx/cmx-env-glib.h
+++ cmx/cmx-env-glib.h
@@ -21,56 +21,56 @@
 #define CMX_ENV_GLIB_MUTEX_INIT(Var)                                  \
     { 0 }
 
-#  if CMX_MUTEX_INIT
+#  ifndef CMX_MUTEX_INIT
 #  define CMX_MUTEX_INIT CMX_ENV_GLIB_MUTEX_INIT
 #  endif
 
 #define CMX_ENV_GLIB_MUTEX_LOCK(Var)                                    \
     g_mutex_lock (& (Var))
 
-#  if CMX_MUTEX_LOCK
+#  ifndef CMX_MUTEX_LOCK
 #  define CMX_MUTEX_LOCK CMX_ENV_GLIB_MUTEX_LOCK
 #  endif
 
 #define CMX_ENV_GLIB_MUTEX_UNLOCK(Var)                                  \
     g_mutex_unlock (& (Var))
 
-#  if CMX_ENV_BIND_GLIB_P (MUTEX_UNLOCK)
+#  ifndef CMX_ENV_GLIB_MUTEX_UNLOCK
 #  define CMX_MUTEX_UNLOCK CMX_ENV_GLIB_MUTEX_UNLOCK
 #  endif
 
 #define CMX_ENV_GLIB_ATOMIC_INT_TYPE                                    \
     gint
 
-#  if CMX_ATOMIC_INT_TYPE
+#  ifndef CMX_ATOMIC_INT_TYPE
 #  define CMX_ATOMIC_INT_TYPE CMX_ENV_GLIB_ATOMIC_INT_TYPE
 #  endif
 
 #define CMX_ENV_GLIB_ATOMIC_INT_SET(Var, Value)                         \
     g_atomic_int_set (& (Var), (Value))
 
-#  if CMX_ATOMIC_INT_SET
+#  ifndef CMX_ATOMIC_INT_SET
 #  define CMX_ATOMIC_INT_SET CMX_ENV_GLIB_ATOMIC_INT_SET
 #  endif
 
 #define CMX_ENV_GLIB_ATOMIC_INT_INCREMENT(Var)                          \
     g_atomic_int_inc (& (Var))
 
-#  if CMX_ATOMIC_INT_INCREMENT
+#  ifndef CMX_ATOMIC_INT_INCREMENT
 #  define CMX_ATOMIC_INT_INCREMENT CMX_ENV_GLIB_ATOMIC_INT_INCREMENT
 #  endif
 
 #define CMX_ENV_GLIB_ATOMIC_INT_DECREMENT_AND_TEST(Var)                 \
     g_atomic_int_dec_and_test (& (Var))
 
-#  if CMX_ATOMIC_INT_DECREMENT_AND_TEST
+#  ifndef CMX_ATOMIC_INT_DECREMENT_AND_TEST
 #  define CMX_ATOMIC_INT_DECREMENT_AND_TEST CMX_ENV_GLIB_ATOMIC_INT_DECREMENT_AND_TEST
 #  endif
 
 #define CMX_ENV_GLIB_LABEL_UNUSED                                       \
     G_GNUC_UNUSED
 
-#  if CMX_ENV_BIND_GLIB_P (LABEL_UNUSED)
+#  ifndef CMX_LABEL_UNUSED
 #  define CMX_LABEL_UNUSED CMX_ENV_GLIB_LABEL_UNUSED
 #  endif
 
